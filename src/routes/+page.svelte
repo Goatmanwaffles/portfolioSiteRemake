5161b3deb15f42927e8999f29eaf74f4affbd7d0
Logan <goatmanwaffles@gmail.com>
2026-02-16 14:18:43 -0500

    Added flash effect to hero buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,8 +9,14 @@
     <h1 class="text-5xl font-bold text-pink-500 col-span-2">Logan Senol</h1>
     <p class="text-gray-400 text-xl font-bold">A Full Stack Developer</p>
     <div class="flex md:flex-row gap-32 flex-col">
-      <a href="/about" class="rounded-full bg-pink-400 px-14 py-2 hover:bg-pink-600 transition-colors duration-500">&lt;About me&gt;</a>
-      <a href="/projects" class="rounded-full bg-pink-400 px-14 py-2 hover:bg-pink-600 transition-colors duration-500">&lt;My Projects&gt;</a>
+      <div class="group relative rounded-full overflow-hidden">
+        <span class="pointer-events-none absolute inset-0 bg-gradient-to-r from-transparent via-white/40 to-transparent -translate-x-full opacity-0 group-hover:animate-flash z-20"></span>
+        <a href="/about" class="relative inline-block rounded-full bg-pink-400 px-14 py-2 hover:bg-pink-600 transition-colors duration-500 z-10">&lt;About Me&gt;</a>
+      </div>
+      <div class="group relative rounded-full overflow-hidden">
+        <span class="pointer-events-none absolute inset-0 bg-gradient-to-r from-transparent via-white/40 to-transparent -translate-x-full opacity-0 group-hover:animate-flash z-20"></span>
+        <a href="/projects" class="relative inline-block rounded-full bg-pink-400 px-14 py-2 hover:bg-pink-600 transition-colors duration-1000 z-10">&lt;My Projects&gt;</a>
+      </div>
     </div>
   </div>
 </div>
