e57cd1fcde8ba4a6aaf05f902aef3efea5572b32
Logan <goatmanwaffles@gmail.com>
2026-02-23 13:38:33 -0500

    FIXED HERO AGHAIN AHHHH

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,12 +10,12 @@
     <p class="text-gray-400 text-xl font-bold">A Full Stack Developer</p>
     <div class="flex md:flex-row gap-32 flex-col">
       <div class="group relative rounded-full overflow-hidden">
-        <span class="pointer-events-none absolute inset-0 bg-gradient-to-r from-transparent via-white/40 to-transparent -translate-x-full opacity-0 group-hover:animate-flash z-20"></span>
-        <a href="/about" class="relative inline-block rounded-full bg-pink-400 px-14 py-2 hover:bg-pink-600 transition-colors duration-500 z-10">&lt;About Me&gt;</a>
+        <span class="pointer-events-none absolute inset-0 bg-gradient-to-r from-transparent via-white/40 to-transparent -translate-x-full opacity-0 group-hover:animate-flash z-30 duration-1000"></span>
+        <a href="/about" class="relative inline-block rounded-full bg-pink-400 px-14 py-2 hover:bg-pink-600 transition-colors duration-1000 z-20 ">&lt;About Me&gt;</a>
       </div>
       <div class="group relative rounded-full overflow-hidden">
-        <span class="pointer-events-none absolute inset-0 bg-gradient-to-r from-transparent via-white/40 to-transparent -translate-x-full opacity-0 group-hover:animate-flash z-20"></span>
-        <a href="/projects" class="relative inline-block rounded-full bg-pink-400 px-14 py-2 hover:bg-pink-600 transition-colors duration-1000 z-10">&lt;My Projects&gt;</a>
+        <span class="pointer-events-none absolute inset-0 bg-gradient-to-r from-transparent via-white/40 to-transparent -translate-x-full opacity-0 group-hover:animate-flash z-30 duration-1000"></span>
+        <a href="/projects" class="relative inline-block rounded-full bg-pink-400 px-14 py-2 hover:bg-pink-600 transition-colors duration-1000 z-20 ">&lt;My Projects&gt;</a>
       </div>
     </div>
   </div>
